685d60fb7132dacf6e1879e16285e79f8aeecac6
samyralencar <64990711+samyralencar@users.noreply.github.com>
2022-12-10 11:22:02 -0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -12,7 +12,7 @@ const countdownfunction = setInterval(function () {
   const seconds = Math.floor(diff % (1000 * 60) / 1000);
 
   if (diff < 0) {
-    //clockdiv.style.display = "none";
+    clockdiv.style.display = "none";
     clearInterval(countdownfunction);
   } else {
     clockdiv.querySelector(".days").innerHTML = days;
